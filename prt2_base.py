f485e77cd312b5b5438b9682d34742ed6849b5e7
dsejm <dsejmovskij@gmail.com>
2023-07-05 18:07:57 +0300

    fix prt2

--- prt2_base.py
+++ prt2_base.py
@@ -1,19 +1,20 @@
 import json
 class Model:
-    title = "title"
-    text = "text"
-    author = "author"
-
-
-    def save(self):
+    title = input("Введите заголовок: ")
+    text = input("Введите текст сообщения: ")
+    author = input("Введите автора сообщения: ")
+    def _save(self):
         with open(f"model_attributes_{self.author}.json", "w") as file:
             list_atr = list(filter(lambda x: not x.startswith('_'), dir(Model)))
-            list_atr.remove("save")
             list_atr = list_atr[::-1]
             list_value = []
-            list_value.append(self.title)
-            list_value.append(self.text)
-            list_value.append(self.author)
+            for i in list_atr:
+                value = getattr(Model, f"{i}")
+                list_value.append(value)
             data = dict(zip(list_atr, list_value))
+            print(data)
             json.dump(data, file)
             print("Data save!")
+
+state = Model()
+state._save()
